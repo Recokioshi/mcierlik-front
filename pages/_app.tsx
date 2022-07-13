30113596dbf40bc96b9845cc28702b507400a1fe
Adrian Gaik <Adrian.Gaik@esatto.se>
2022-07-13 11:18:47 +0200

    - added basic navigation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import '../styles/globals.css'
 import './products/products.css'
 import type { AppProps } from 'next/app'
+import { Navbar } from '../components/Navbar/Navbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
