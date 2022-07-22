404fdd8bb6adfc95cae26b2fe4161c772a2f449a
Adrian Gaik <recokioshi@gmail.com>
2022-07-22 16:10:26 +0200

    feature/contact-form-sends-email - added send button functionality to contact - added api function to handle email sending

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import {
   createStyles,
   Text,
@@ -10,9 +10,15 @@ import {
   Group,
   Container,
   Space,
+  Notification,
+  Popper,
+  useMantineTheme,
 } from '@mantine/core';
 import { ContactIconsList } from '../../components/Contact/ContactIconsList';
 import { SocialMediaLinks } from '../../components/SocialMediaLinks';
+import { Check, X } from 'tabler-icons-react';
+import { useClickOutside } from '@mantine/hooks';
+
 
 const useStyles = createStyles((theme) => ({
   wrapper: {
@@ -78,8 +84,62 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
+const onSendEmail = async (email: string, name: string, message: string) => {
+  return await fetch('/api/email', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email,
+      name,
+      message,
+    }),
+  });
+};
+  
+
 function ContactUs() {
   const { classes } = useStyles();
+  const theme = useMantineTheme();
+
+  const [email, setEmail] = React.useState('');
+  const [name, setName] = React.useState('');
+  const [message, setMessage] = React.useState('');
+  const [error, setError] = React.useState('');
+  const [sentSuccessfully, setSentSuccessfully] = React.useState(false);
+
+  const buttonRef = useClickOutside(() => {
+    setSentSuccessfully(false);
+    setError('');
+  });
+
+  const handleSubmit = useCallback(() => {
+    if (!email || !name || !message) {
+      setError('Please fill out all fields');
+      return;
+    }
+    setError('');
+    setSentSuccessfully(true);
+    onSendEmail(email, name, message).then((res) => {
+    if(res.status === 200) {
+      setEmail('');
+      setName('');
+      setMessage('');
+      setSentSuccessfully(true);
+    }
+    }).catch((err) => {
+      console.log(error);
+      setError('Bummer! Something went wrong, and the message was not sent.');
+    });
+  }, [email, name, message, error]);
+
+  const onInputChange = useCallback(
+    (fieldSetter: React.Dispatch<React.SetStateAction<string>>) => 
+      (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        fieldSetter(event.target.value);
+      }
+    , []);
 
   return (
     <Container mt={30} mb={30} size="lg">
@@ -102,12 +162,17 @@ function ContactUs() {
               placeholder="your@email.com"
               required
               classNames={{ input: classes.input, label: classes.inputLabel }}
+              value={email}
+              onChange={onInputChange(setEmail)}
             />
             <TextInput
               label="Name"
               placeholder="John Doe"
               mt="md"
               classNames={{ input: classes.input, label: classes.inputLabel }}
+              value={name}
+              onChange={onInputChange(setName)}
+              required
             />
             <Textarea
               required
@@ -116,11 +181,33 @@ function ContactUs() {
               minRows={4}
               mt="md"
               classNames={{ input: classes.input, label: classes.inputLabel }}
+              value={message}
+              onChange={onInputChange(setMessage)}
             />
 
             <Group position="right" mt="md">
-              <Button className={classes.control}>Send message</Button>
+              <Button className={classes.control} onClick={handleSubmit} ref={buttonRef}>Send message</Button>
             </Group>
+            <Popper
+              position="bottom"
+              placement="end"
+              arrowSize={0}
+              mounted={error !== '' || sentSuccessfully}
+              referenceElement={buttonRef.current!}
+              transition="pop-top-left"
+              transitionDuration={200}
+            >
+              {error && 
+                <Notification icon={<X size={18} />} color="red" disallowClose={true}>
+                  {error}
+                </Notification>
+              }
+              {sentSuccessfully &&
+                <Notification icon={<Check size={18} />} color="teal" title="Success!" disallowClose={true}>
+                  You have left the message. We will contact you as soon as possible.
+                </Notification>
+              }
+            </Popper>            
           </div>
         </SimpleGrid>
       </div>
